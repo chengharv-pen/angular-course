d7930119b97189c082d2ef947269640e29846358
chengharv-pen <chengharvp@gmail.com>
2025-05-10 16:55:04 -0400

    README.md formatting

---
+++ 09-services-DI-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, Inject, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
+import { TasksServiceToken } from '../../../main';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  // private tasksService: TasksService;
+  // constructor(tService: TasksService) {
+  //   this.tasksService = tService;
+  // }
+
+  // constructor(private tasksService: TasksService) {}
+  constructor(@Inject(TasksServiceToken) private tasksService: TasksService) {}
+
+  onAddTask(title: string, description: string) {
+    this.tasksService.addTask({ title: title, description: description });
+
+    this.formEl()?.nativeElement.reset();
+  }
+}
