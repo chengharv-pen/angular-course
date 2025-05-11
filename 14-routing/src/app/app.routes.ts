54bf2b5c0b5a32b7cc220727acd272ee32e7d3e2
chengharv-pen <chengharvp@gmail.com>
2025-05-11 17:40:40 -0400

    section 14

---
+++ 14-routing/src/app/app.routes.ts
@@ -0,0 +1,50 @@
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import {
+  resolveTitle,
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { routes as userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+
+  // ultimately, a boolean OR a RedirectCommand should be returned
+  // true -> granted access
+  // false -> denied access
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
+
+export const routes: Routes = [
+  {
+    path: '', // <your-domain>/
+    component: NoTaskComponent,
+    // redirectTo: '/users/u1',
+    // pathMatch: 'full',
+    title: 'No task selected',
+  },
+  {
+    path: 'users/:userId', // <your-domain>/users/<userId>
+    component: UserTasksComponent,
+    children: userRoutes,
+    // canMatch: [dummyCanMatch],
+    data: {
+      message: 'Hello!',
+    },
+    resolve: {
+      userName: resolveUserName,
+    },
+    title: resolveTitle,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  private usersService = inject(UsersService);
+  message = input.required<string>();
+
+  userName = input.required<string>();
+  // private activatedRoute = inject(ActivatedRoute);
+  // ngOnInit(): void {
+  //   // holds an Observable that has static and resolved data.
+  //   this.activatedRoute.data.subscribe({
+  //     next: (data) => {
+  //       console.log(data);
+  //     },
+  //   });
+  // }
+
+  //
+  // OTHER APPROACHES SIMILAR TO THE EXPORTED FUNCTION
+  //
+  // APPROACH 1
+  //
+  // userId = input.required<string>();
+  // userName = computed(
+  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  // );
+
+  // APPROACH 2
+  //
+  // private activatedRoute = inject(ActivatedRoute);
+  // userName = '';
+  // private destroyRef = inject(DestroyRef);
+  // ngOnInit(): void {
+  //   console.log('Input Data: ' + this.message());
+  //   console.log(this.activatedRoute.snapshot);
+  //   console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: (paramMap) => {
+  //       this.userName =
+  //         this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+  //           ?.name || '';
+  //     },
+  //   });
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      (u) => u.id === activatedRoute.paramMap.get('userId')
+    )?.name || '';
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  return resolveUserName(activatedRoute, routerState) + "'s Tasks";
+};
